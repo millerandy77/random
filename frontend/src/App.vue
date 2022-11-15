1eb13930440019cbf21976fe08959073c9210ada
jmcg1945 <joe.f.mcgraw@gmail.com>
2022-11-14 21:55:41 -0500

    This is the best update yet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/ping">Ping</router-link>
-  </nav>
-  <router-view/>
+  <body>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/ping">News</router-link></li>  
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+    <router-view/>
+  </body>
 </template>
 
 <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
